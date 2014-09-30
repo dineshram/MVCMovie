2f3e7a8cc73c28f329a7534f649dea949b155ae2
dineshram <dinesh.superman2006@gmail.com>
2014-09-30 13:55:04 +0200

    MVC Tutorial
    Following tutorial from
    http://www.asp.net/mvc/tutorials/mvc-5/introduction

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+
+        public string Index()
+        {
+            return "<b>default</b> action";
+        }
+
+        // GET: /HelloWorld/Welcome
+
+        public string Welcome()
+        {
+            return "The welcome action method..";
+        }
+      /*  public ActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
