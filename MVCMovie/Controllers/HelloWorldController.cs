4d0d3dbbff6d0f8b78dda288814586215cfcfbd3
dineshram <dinesh.superman2006@gmail.com>
2014-09-30 16:26:27 +0200

    Added a new view
    Using a Razor View Engine.
    A view is a .cshtml file
    Here "the HelloWorldController class to use view template files to
    cleanly encapsulate the process of generating HTML responses to a
    client."

--- MVCMovie/Controllers/HelloWorldController.cs
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -7,9 +7,9 @@ namespace MVCMovie.Controllers
     {
         // GET: HelloWorld
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "<b>default</b> action";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome
